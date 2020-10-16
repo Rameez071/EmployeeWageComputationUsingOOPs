87fe27abf87c6f17faea7b806e5dfeba6d6c3b4c
Rameez071 <rameezraja405@gmail.com>
2020-10-16 10:42:57 +0530

    Refactor : code refactored to switch case functionality

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,17 +9,20 @@ class checkAttendance extends calculateWage{
 	//Computation
     	double empCheck = Math.floor(Math.random() * 10) % 3;
         void isPresent(){
-        if ( check == 0 ) {
-            System.out.println("Employee is Present for full time !");
-            System.out.println("Wage is "+dailyWage(8));
+        switch(check) {
+            case 0:
+                System.out.println("Employee is Present for full time !");
+                System.out.println("Wage is " + dailyWage(8));
+                break;
+            case 1:
+                System.out.println("Employee is Present for part time !");
+                System.out.println("Wage is " + dailyWage(4));
+                break;
+            default:
+                System.out.println("Employee is Absent !");
+                break;
         }
-        else if ( check == 1 ) {
-            System.out.println("Employee is Present for part time !");
-            System.out.println("Wage is "+dailyWage(4));
-        }
-        else
-            System.out.println("Employee is Absent !");
-}
+    } 
 }
 
 //Wage computation 
